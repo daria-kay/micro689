6a5246ec3745951a77e7caa8581d5c7d6b30db8d
Daria <daria.kay000@gmail.com>
2019-10-14 20:17:47 +0500

    initial commit

---
+++ src/main/java/com/darakay/micro689/Micro689Application.java
@@ -0,0 +1,13 @@
+package com.darakay.micro689;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro689Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro689Application.class, args);
+	}
+
+}
